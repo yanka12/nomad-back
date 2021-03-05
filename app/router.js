b8fc72deb8345b21fea9cd39c35bb1580e963425
yanka12 <guillaume.cuvillers@gmail.com>
2021-03-05 10:47:15 +0100

    supp rte 404

--- app/router.js
+++ app/router.js
@@ -4,10 +4,6 @@ const router = Router();
 
 router.get('/coucou', (req, res) => {
     res.json({hello :"coucou"})
-
-    // ici, une 404 pour l'API
-router.use((request, response) => {
-    response.status(404).json('No such endpoint');
 });
 
 
