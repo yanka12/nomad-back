debfa7507a027c2892e49ce26b531fbd1aca0ec9
yanka12 <guillaume.cuvillers@gmail.com>
2021-03-05 23:00:56 +0100

    add sql file + md + code comment

---
+++ app/data/insert.sql
@@ -0,0 +1,28 @@
+-- insertion de données dans person
+
+INSERT INTO person ("nickname","email","password") VALUES 
+('Audrey', 'Reba16@gmail.com', 'Audy0714',),
+('ARNOULD', 'Kiehn@yahoo.com', 'jean-francois-arnould'),
+('Yunxi', 'Crawford53@hotmail.com', 'YunxiBZ'),
+('Theo', 'Malvina_Wehner@hotmail.com', 'TheoB-png'),
+('François', 'Larue52@gmail.com', 'FrancoisB75'),
+('Diane', 'Marlene.Gislason16@hotmail.com', 'DianeDianeDiane'),
+('Guillaume', 'Dock_Yost73@gmail.com', 'GuillaumeBB69'),
+('Romain', 'Stella23@gmail.com', 'RomainBoudet'),
+('Franck', 'Tracy_Kiehn11@yahoo.com', 'FranckCapon'),
+('Ludivine', 'Junius.Hodkiewicz@yahoo.com', 'ludivineConstanti'),
+('Wladimir', 'Katlynn_Beahan39@yahoo.com', 'Muggen30'),
+('Boris', 'Dale7@hotmail.com', 'boris-couderc'),
+('Anthony', 'Dakota.Homenick16@hotmail.com', 'mrz1880'),
+('Stéphane', 'Kattie.Morissette@gmail.com', 'StephaneD94'),
+('Jérôme', 'Aliyah_Boyer59@hotmail.com', 'LeJ84'); 
+
+
+-- insertion de données dans person
+
+INSERT INTO catégory ("name") VALUES 
+('marcheur'),
+('van'),
+('bateau');
+
+
