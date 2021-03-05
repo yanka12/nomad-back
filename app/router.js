1d787dd7b871f36812eb65884caae595c2b81623
yanka12 <guillaume.cuvillers@gmail.com>
2021-03-05 10:37:23 +0100

    ajout 404

--- app/router.js
+++ app/router.js
@@ -4,6 +4,10 @@ const router = Router();
 
 router.get('/coucou', (req, res) => {
     res.json({hello :"coucou"})
+
+    // ici, une 404 pour l'API
+router.use((request, response) => {
+    response.status(404).json('No such endpoint');
 });
 
 
