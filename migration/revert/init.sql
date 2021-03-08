b05388179d189a010ef3b7cafa902b2fffe0c549
yanka12 <guillaume.cuvillers@gmail.com>
2021-03-08 15:22:00 +0100

    modif files

--- migration/revert/init.sql
+++
@@ -1,7 +0,0 @@
--- Revert nomad:init from pg
-
-BEGIN;
-
-DROP TABLE have, article_media, person_media, "role", category_article, comment, media, category, article, person;
-
-COMMIT;
--- migration/verify/init.sql
+++
@@ -1,7 +0,0 @@
--- Verify digitalNomad:init on pg
-
-BEGIN;
-
--- XXX Add verifications here.
-
-ROLLBACK;
