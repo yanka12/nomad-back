1d940d75c24b0ad84945d414ce446bc32b0ef487
yanka12 <guillaume.cuvillers@gmail.com>
2021-03-12 12:09:09 +0100

    modif isNomad

--- app/middlewares/isNomad.js
+++ app/middlewares/isNomad.js
@@ -1,13 +1,14 @@
-const isAdmin = async (req, res, next) => {
+const isNomad = async (req, res, next) => {
     // si l'utilisateur n'a pas le rôle admin
     // on rend la vue 403
     //console.log(req.session.role_id);
-    if (req.session.user.role_id !== 2) {
-    
+    if (req.session.user.role_id == 2 || req.session.user.role_id == 1 ) {
+        next();
+    }
+    else{
     await res.status(403).json({"error":"not authorized"});
     }
+    
+};
 
-    next();
-}
-
-module.exports = isAdmin;
+module.exports = isNomad;
